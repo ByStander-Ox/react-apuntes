e17e6f084a246b6fb51480189a50e5e8bac71dd9
Majo Ledesma <majoledesma@MacBook-Pro-de-Nacho.local>
2020-01-13 00:41:34 -0300

    Updates

---
+++ precache-manifest.edce9d0b0cc66a4555e8684216b5edc7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0778ef88d3f74ce7e4492498b4fc3996",
+    "url": "/react-apuntes/index.html"
+  },
+  {
+    "revision": "49fbea931535419e1ea4",
+    "url": "/react-apuntes/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "6bc352555c9ea0b16588",
+    "url": "/react-apuntes/static/css/main.98828d1a.chunk.css"
+  },
+  {
+    "revision": "49fbea931535419e1ea4",
+    "url": "/react-apuntes/static/js/2.f3758b46.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-apuntes/static/js/2.f3758b46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bc352555c9ea0b16588",
+    "url": "/react-apuntes/static/js/main.90c8c9f3.chunk.js"
+  },
+  {
+    "revision": "0efb5312115cfc044e94",
+    "url": "/react-apuntes/static/js/runtime-main.d8401d36.js"
+  },
+  {
+    "revision": "1eb45a0ba9fe47087cf69068caec1eb9",
+    "url": "/react-apuntes/static/media/book2.1eb45a0b.svg"
+  },
+  {
+    "revision": "5cff78e44ca42e5026f832f7b41fb018",
+    "url": "/react-apuntes/static/media/github.5cff78e4.svg"
+  },
+  {
+    "revision": "7c6e51dced0af371e3b62c5b4ddb9f51",
+    "url": "/react-apuntes/static/media/instagram.7c6e51dc.svg"
+  },
+  {
+    "revision": "43449f1c48de00f0ef903fcacf487626",
+    "url": "/react-apuntes/static/media/twitter.43449f1c.svg"
+  }
+]);
